c957e0c1dec0abd1272bdfe7203ee881b0389e34
junggunlee <leejunggun7@gmail.com>
2017-10-11 15:36:54 +0900

    make a review form inside the restaurant page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,12 +1,12 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
 
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
-    # byebug
+    @review = Review.new
   end
 
   def new
@@ -23,6 +23,10 @@ class RestaurantsController < ApplicationController
   end
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,8 @@
 class ReviewsController < ApplicationController
-
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
+  before_action :set_restaurant, only: [:new, :create]
 
   def create
     @review = Review.new(review_params)
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
@@ -17,6 +12,10 @@ class ReviewsController < ApplicationController
   end
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
